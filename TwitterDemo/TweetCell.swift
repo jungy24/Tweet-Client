68dc8741c03556acb6a63dc338b6ab4db24f6cf1
Jungyoon Yu <yu612@purdue.edu>
2017-02-27 23:29:14 -0500

    first commit

---
+++ TwitterDemo/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  TwitterDemo
+//
+//  Created by Jungyoon Yu on 2/27/17.
+//  Copyright © 2017 Jungyoon Yu. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TwitterDemo/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  TwitterDemo
+//
+//  Created by Jungyoon Yu on 2/27/17.
+//  Copyright © 2017 Jungyoon Yu. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1SessionManager {
+
+}
